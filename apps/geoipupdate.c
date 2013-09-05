583b2def9edb4f734cee4dfdb59611496de57fe0
Boris Zentner <bzentner@maxmind.com>
2013-09-05 16:09:35 +0200

    Add geoipupdate.c

---
+++ apps/geoipupdate.c
@@ -0,0 +1,22 @@
+
+#include "geoipupdate.h"
+#include <curl/curl.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
+#include <ctype.h>
+
+#define PRODUCT_ID_TOKEN "ProductIds"
+#define USER_ID_TOKEN "UserId"
+#define LICENSE_KEY_TOKEN "LicenseKey"
+#define LICENSE_KEY_LENGTH 12
+
+int main(int argc, const char *argv[])
+{
+	
+	return 0;
+}
