84aa60830c9d97f14020d4281e649ffeeef1aad2
Boris Zentner <bzentner@maxmind.com>
2013-09-06 17:01:48 +0200

    Rename product_id_s to product_s

--- apps/geoipupdate.h
+++ apps/geoipupdate.h
@@ -4,15 +4,15 @@
 
 #include <curl/curl.h>
 
-typedef struct product_id_s {
+typedef struct product_s {
     char *product_id;
-    struct product_id_s *next;
-} product_id_s;
+    struct product_s *next;
+} product_s;
 
 typedef struct {
     int user_id;
     char license_key[12];
-    product_id_s *first;
+    product_s *first;
 } license_s;
 
 typedef struct {
@@ -28,6 +28,5 @@ geoipupdate_s * geoipupdate_s_new(void);
 void geoipupdate_s_delete(geoipupdate_s * gu);
 int geoipupdate_s_init(geoipupdate_s * gu);
 void geoipupdate_s_cleanup(geoipupdate_s * gu);
-
 
 #endif
