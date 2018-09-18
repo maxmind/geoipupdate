5d524d44bffd4eeac03e829358f2271e42126e65
William Storey <wstorey@maxmind.com>
2018-09-18 22:33:22 +0000

    Default to the paths set if built from source

--- cmd/geoipupdate/defaults_notwin.go
+++ cmd/geoipupdate/defaults_notwin.go
@@ -7,6 +7,7 @@ package main
 // go build -ldflags "-X main.defaultConfigFile=/usr/local/etc/GeoIP.conf
 // -X main.defaultDatabaseDirectory=/usr/local/share/GeoIP"
 var (
-	defaultConfigFile        = "/etc/GeoIP.conf"
-	defaultDatabaseDirectory = "/usr/share/GeoIP"
+	// These match what you'd get building the C geoipupdate from source.
+	defaultConfigFile        = "/usr/local/etc/GeoIP.conf"
+	defaultDatabaseDirectory = "/usr/local/share/GeoIP"
 )
