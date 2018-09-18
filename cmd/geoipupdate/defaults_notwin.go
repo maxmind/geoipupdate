9450d1660ef93215db646f1b3511e65d45808001
William Storey <wstorey@maxmind.com>
2018-09-18 22:37:38 +0000

    Remove redundant comments

--- cmd/geoipupdate/defaults_notwin.go
+++ cmd/geoipupdate/defaults_notwin.go
@@ -2,10 +2,6 @@
 
 package main
 
-// These can be changed using compile time flags. e.g.,
-//
-// go build -ldflags "-X main.defaultConfigFile=/usr/local/etc/GeoIP.conf
-// -X main.defaultDatabaseDirectory=/usr/local/share/GeoIP"
 var (
 	// These match what you'd get building the C geoipupdate from source.
 	defaultConfigFile        = "/usr/local/etc/GeoIP.conf"
--- cmd/geoipupdate/defaults_windows.go
+++ cmd/geoipupdate/defaults_windows.go
@@ -4,10 +4,6 @@ import (
 	"os"
 )
 
-// These can be changed using compile time flags. e.g.,
-//
-// go build -ldflags "-X main.defaultConfigFile=/usr/local/etc/GeoIP.conf
-// -X main.defaultDatabaseDirectory=/usr/local/share/GeoIP"
 var (
 	// I'm not sure these make sense. However they can be overridden at runtime
 	// and in the configuration, so we have some flexibility.
