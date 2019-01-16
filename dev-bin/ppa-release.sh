e61a55e45111447105b7849ce600e173c5110c56
Will Storey <will@summercat.com>
2019-01-15 17:53:15 -0800

    Support additional dist and use correct changelog

--- dev-bin/ppa-release.sh
+++ dev-bin/ppa-release.sh
@@ -4,11 +4,11 @@ set -e
 set -x
 set -u
 
-DISTS=( bionic xenial trusty )
+DISTS=( cosmic bionic xenial trusty )
 
 VERSION=$(perl -MFile::Slurp::Tiny=read_file -MDateTime <<EOF
 use v5.16;
-my \$log = read_file(q{ChangeLog.md});
+my \$log = read_file(q{CHANGELOG.md});
 \$log =~ /\n(\d+\.\d+\.\d+) \((\d{4}-\d{2}-\d{2})\)\n/;
 die 'Release time is not today!' unless DateTime->now->ymd eq \$2;
 say \$1;
