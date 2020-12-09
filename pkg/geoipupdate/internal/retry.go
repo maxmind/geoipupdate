be18fd0f19ae2cd205fdb9b7b1eee935fd65e0cf
William Storey <wstorey@maxmind.com>
2020-12-09 12:27:23 -0800

    Retry entire HTTP exchange when retrieving filename
    Previously we did not retry reading from the body for example.

--- pkg/geoipupdate/internal/retry.go
+++ pkg/geoipupdate/internal/retry.go
@@ -45,3 +45,29 @@ func MaybeRetryRequest(c *http.Client, retryFor time.Duration, req *http.Request
 	}
 	return resp, nil
 }
+
+// RetryWithBackoff calls the provided function repeatedly until it succeeds or
+// until the retry duration is up.
+func RetryWithBackoff(
+	fn func() error,
+	retryFor time.Duration,
+) error {
+	start := time.Now()
+
+	for i := uint(0); ; i++ {
+		err := fn()
+		if err == nil {
+			return nil
+		}
+
+		currentDuration := time.Since(start)
+
+		waitDuration := 200 * time.Millisecond * (1 << i)
+
+		if currentDuration+waitDuration > retryFor {
+			return err
+		}
+
+		time.Sleep(waitDuration)
+	}
+}
