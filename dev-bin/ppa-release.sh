7bcab327dd6d574eb8b0de274d6a837228e8f86b
Gregory Oschwald <goschwald@maxmind.com>
2022-02-15 15:50:55 -0800

    Do not use File::Slurp::Tiny

--- dev-bin/ppa-release.sh
+++ dev-bin/ppa-release.sh
@@ -6,9 +6,9 @@ set -u
 
 DISTS=( impish groovy focal bionic )
 
-VERSION=$(perl -MFile::Slurp::Tiny=read_file -MDateTime <<EOF
+VERSION=$(perl -MFile::Slurper=read_text -MDateTime <<EOF
 use v5.16;
-my \$log = read_file(q{CHANGELOG.md});
+my \$log = read_text(q{CHANGELOG.md});
 \$log =~ /\n## (\d+\.\d+\.\d+) \((\d{4}-\d{2}-\d{2})\)\n/;
 die 'Release time is not today!' unless DateTime->now->ymd eq \$2;
 say \$1;
