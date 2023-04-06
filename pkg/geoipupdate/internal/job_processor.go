b32ee40f59fad0bd1de066622b30778036e745b2
Naji Obeid <nobeid@maxmind.com>
2023-04-06 11:20:33 -0400

    add file_lock and job_processor components to geoipupdate/internal

---
+++ pkg/geoipupdate/internal/job_processor.go
@@ -0,0 +1,76 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// JobProcessor runs jobs with a set number of workers.
+type JobProcessor struct {
+	// sync.Mutex prevents adding new jobs while the processor is running.
+	sync.Mutex
+	// processor is used to parallelize and limit the number of workers
+	// processing read requests.
+	processor *errgroup.Group
+	// jobs defines the jobs to be processed.
+	jobs []func(context.Context) error
+	// cancel cancels the context and stops the processing of the queue.
+	cancel context.CancelFunc
+}
+
+// NewJobProcessor inits a new JobProcessor struct.
+func NewJobProcessor(ctx context.Context, workers int) *JobProcessor {
+	processor, _ := errgroup.WithContext(ctx)
+	processor.SetLimit(workers)
+
+	return &JobProcessor{
+		processor: processor,
+		jobs:      []func(context.Context) error{},
+	}
+}
+
+// Add queues a job for processing.
+func (j *JobProcessor) Add(job func(context.Context) error) {
+	j.Lock()
+	defer j.Unlock()
+
+	j.jobs = append(j.jobs, job)
+}
+
+// Run processes the job queue and returns the first error encountered, if any.
+func (j *JobProcessor) Run(ctx context.Context) error {
+	j.Lock()
+	defer j.Unlock()
+
+	ctx, j.cancel = context.WithCancel(ctx)
+	for _, job := range j.jobs {
+		job := job
+		j.processor.Go(func() error {
+			if err := ctx.Err(); err != nil {
+				return fmt.Errorf("processing cancelled: %w", err)
+			}
+			return job(ctx)
+		})
+	}
+
+	return j.Wait()
+}
+
+// Wait waits for all jobs to finish processing and returns the first
+// error encountered, if any.
+func (j *JobProcessor) Wait() error {
+	if err := j.processor.Wait(); err != nil {
+		return fmt.Errorf("error running job: %w", err)
+	}
+	return nil
+}
+
+// Stop cancels all queued jobs.
+func (j *JobProcessor) Stop() {
+	if j.cancel != nil {
+		j.cancel()
+	}
+}
---
+++ pkg/geoipupdate/internal/job_processor_test.go
@@ -0,0 +1,121 @@
+package internal
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestJobQueueRun tests the parallel job queue functionality
+// and ensures that the maximum number of allowed goroutines does not exceed the value
+// set in the config.
+func TestJobQueueRun(t *testing.T) {
+	simulatedJobDuration := 5 * time.Millisecond
+	jobsNumber := 10
+
+	tests := []struct {
+		Description string
+		Parallelism int
+	}{{
+		Description: "sequential jobs",
+		Parallelism: 1,
+	}, {
+		Description: "parallel jobs",
+		Parallelism: 3,
+	}}
+
+	for _, test := range tests {
+		t.Run(test.Description, func(t *testing.T) {
+			doneCh := make(chan struct{})
+			var lock sync.Mutex
+			runningGoroutines := 0
+			maxConcurrentGoroutines := 0
+
+			// A mock processor function that is used to gather data
+			// about the number of goroutines called.
+			processorFunc := func(_ context.Context) error {
+				lock.Lock()
+				runningGoroutines++
+				if runningGoroutines > maxConcurrentGoroutines {
+					maxConcurrentGoroutines = runningGoroutines
+				}
+				lock.Unlock()
+
+				time.Sleep(simulatedJobDuration)
+
+				lock.Lock()
+				runningGoroutines--
+				lock.Unlock()
+				return nil
+			}
+
+			ctx := context.Background()
+			jobProcessor := NewJobProcessor(ctx, test.Parallelism)
+			for i := 0; i < jobsNumber; i++ {
+				jobProcessor.Add(processorFunc)
+			}
+
+			// Execute run in a goroutine so that we can exit early if the test
+			// hangs or takes too long to execute.
+			go func() {
+				err := jobProcessor.Run(ctx)
+				require.NoError(t, err)
+				close(doneCh)
+			}()
+
+			// Wait for run to complete or timeout after a certain duration
+			select {
+			case <-doneCh:
+			case <-time.After(1000 * time.Millisecond):
+				t.Errorf("Timeout waiting for function completion")
+			}
+
+			// The maximum number of parallel downloads executed should not exceed
+			// the number defined in the configuration.
+			require.Equal(t, maxConcurrentGoroutines, test.Parallelism)
+		})
+	}
+}
+
+// TestJobQueueStop cancels a job queue and makes sure queued
+// jobs are not processed.
+func TestJobQueueStop(t *testing.T) {
+	doneCh := make(chan struct{})
+	processedJobs := 0
+	maxProcessedJobs := 5
+
+	ctx := context.Background()
+	jobProcessor := NewJobProcessor(ctx, 1)
+
+	processorFunc := func(_ context.Context) error {
+		processedJobs++
+		if processedJobs == maxProcessedJobs {
+			jobProcessor.Stop()
+		}
+		return nil
+	}
+
+	for i := 0; i < 10; i++ {
+		jobProcessor.Add(processorFunc)
+	}
+
+	// Execute run in a goroutine so that we can exit early if the test
+	// hangs or takes too long to execute.
+	go func() {
+		err := jobProcessor.Run(ctx)
+		require.ErrorContains(t, err, "processing cancelled")
+		close(doneCh)
+	}()
+
+	// Wait for run to complete or timeout after a certain duration
+	select {
+	case <-doneCh:
+	case <-time.After(1000 * time.Millisecond):
+		t.Errorf("Timeout waiting for function completion")
+	}
+
+	require.Equal(t, processedJobs, maxProcessedJobs)
+}
